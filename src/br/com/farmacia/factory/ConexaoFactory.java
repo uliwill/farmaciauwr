cc5bb98294a9983e148cfce28303cfc71067fcb8
Ulisses Willian Reichenbach <uliwill@gmail.com>
2021-06-12 23:03:21 -0300

    Insert Fornecedores

--- src/br/com/farmacia/factory/ConexaoFactory.java
+++ src/br/com/farmacia/factory/ConexaoFactory.java
@@ -1,5 +1,26 @@
 package br.com.farmacia.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoFactory {
+	private static final String USUARIO = "root";
+	private static final String SENHA = "";
+	private static final String URL = "jdbc:mysql://localhost:3306/farmacia";
 	
+	public static Connection conectar() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Connection conexao = ConexaoFactory.conectar();
+			System.out.println("Conectado com sucesso!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Erro na conexão com o banco de dados.");
+		}
+	}
 }
